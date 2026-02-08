ae263380c1d19a64bf92175a2eac50978e397bb8
akshay-47-ak <akshayathavale1525@gmail.com>
2026-02-08 14:27:46 +0530

    Created A Project And Start To Build Backend

---
+++ src/main/java/com/project/fitness/model/Activity.java
@@ -0,0 +1,16 @@
+package com.project.fitness.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Activity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+
+}
