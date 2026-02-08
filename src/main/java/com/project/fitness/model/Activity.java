88aadc126fb3fac0eabfcb8c0fbe790662d8ba4d
akshay-47-ak <akshayathavale1525@gmail.com>
2026-02-08 19:48:45 +0530

    Created DataBase And Added the Tables Using The Entity Relations

--- src/main/java/com/project/fitness/model/Activity.java
+++ src/main/java/com/project/fitness/model/Activity.java
@@ -1,9 +1,14 @@
 package com.project.fitness.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Entity
 public class Activity {
@@ -11,6 +16,30 @@ public class Activity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
+
+    // It is used to Store The ENUM Values Not Index ---> EnumType.String
+    @Enumerated(EnumType.STRING)
+    private ActivityType type;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id",nullable = false,foreignKey = @ForeignKey(name = "fk_activity_user "))
+    @JsonIgnore
+    private User user;
+
+    @OneToMany(mappedBy = "activity",cascade = CascadeType.ALL,orphanRemoval = true)
+    @JsonIgnore
+    private List<Recommendation> recommendations = new ArrayList<>();
+
+    //It is used To Store the Data JSON Values into DataBase
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(columnDefinition = "json")
+    private Map<String ,Object> additionalMatrics;
+
+    private Integer duration;
+    private Integer caloriesBruned;
+    private LocalDateTime startTime;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
 
 }
