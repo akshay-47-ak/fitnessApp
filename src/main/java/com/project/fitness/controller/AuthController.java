8b263ac708492a6f0833898cd515f9e52c7b0c17
akshay-47-ak <akshayathavale1525@gmail.com>
2026-02-09 22:11:22 +0530

    Created And Tested The First API(RegisterUser)

--- src/main/java/com/project/fitness/controller/AuthController.java
+++ src/main/java/com/project/fitness/controller/AuthController.java
@@ -1,4 +1,32 @@
 package com.project.fitness.controller;
 
+import com.project.fitness.model.User;
+import com.project.fitness.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor  // used to Create the final Contructor
 public class AuthController {
+
+
+    private final UserService userService;
+
+
+
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(@RequestBody User user){
+
+        User user1 = userService.register(user);
+
+        return new ResponseEntity<>(user1, HttpStatus.CREATED);
+    }
+
 }
